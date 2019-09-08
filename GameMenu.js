9db96cd0a6de900088ae4dee64563b81763f4328
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-08 13:03:06 +0100

    commiting game menu

---
+++ GameMenu.js
@@ -0,0 +1,15 @@
+function addGame() {
+    location.href = 'AddGame.html';
+}
+
+function displayGames() {
+    location.href = 'DisplayGames.html';
+}
+
+function searchGames() {
+    location.href = 'SearchGames.html';
+}
+
+function mainMenu() {
+    location.href = 'HomePage.html';
+}

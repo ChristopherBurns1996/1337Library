8aea9d7fb2ba49349cfa0ed08ba168ee7218e00c
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-08 16:56:41 +0100

    commiting menu page for publisher table

---
+++ PublisherMenu.js
@@ -0,0 +1,11 @@
+function addPublisher(){
+    location.href = "AddPublisher.html";
+}
+
+function displayPublishers() {
+    location.href = 'DisplayPublishers.html';
+}
+
+function mainMenu() {
+    location.href = 'HomePage.html';
+}

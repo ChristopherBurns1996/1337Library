ca03a244cec05fe29e4622b54bab944ed90a1c3d
Chris <Christopher.Burns@academytrainee.com>
2019-09-11 10:16:59 +0100

    committing changes to add game page so form can take in input

--- AddGame.js
+++ AddGame.js
@@ -1,3 +1,29 @@
-function gameMenu() {
-    location.href = 'GameMenu.html';
+function handleFormSubmit(form) {
+
+    const formDataObj = {};
+
+    for (let element of form.elements) {
+        if (element.id) {
+            formDataObj[element.id] = element.value;
+        }
+    }
+
+    const formDataString = JSON.stringify(formDataObj);
+
+    const xhr = new XMLHttpRequest();
+
+    // xhr.onload = () => {
+    //     console.log('load success');
+    //     location.href = "GamesMenu.html";
+    // };
+
+    // xhr.open('POST', "url idk");
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    // xhr.send(formDataString);
+    return false;
 }
+
+function gameMenu() {
+    location.href = "GameMenu.html";
+}
+

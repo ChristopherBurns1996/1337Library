5046fc5378327f165c7593b0689fafa104ff8066
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-18 19:35:05 +0100

    adding beginning of display all games api call

--- DisplayGames.js
+++ DisplayGames.js
@@ -1,12 +1,24 @@
+const xhr = new XMLHttpRequest();
+
+xhr.onload = () => {
+    console.log('load success');
+    data = JSON.parse(xhr.response);
+    console.log(data[10].gameName);
+    document.getElementById("gn").innerHTML = data[10].gameName;
+};
+
+xhr.open('GET', "http://34.89.59.112:9000/Games");
+xhr.send();
+
 function gameMenu() {
     location.href = 'GameMenu.html';
 }
 
-function updateGame(){
+function updateGame() {
     location.href = 'UpdateGame.html';
 }
 
-function deleteGame(){
+function deleteGame() {
     if (confirm('Are you sure you want to delete this game?')) {
         //delete game
     } else {

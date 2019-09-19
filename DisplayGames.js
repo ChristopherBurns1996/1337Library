2c35e1ae5cc6a3f648ad4c0b535434c7e4e1d53e
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-19 20:02:09 +0100

    adding some functionality to display games page

--- DisplayGames.js
+++ DisplayGames.js
@@ -3,8 +3,10 @@ const xhr = new XMLHttpRequest();
 xhr.onload = () => {
     console.log('load success');
     data = JSON.parse(xhr.response);
-    console.log(data[10].gameName);
-    document.getElementById("gn").innerHTML = data[10].gameName;
+    //loops through entire tables data and puts it into table
+    for(let i = 0; i < data.length; i++){
+        newRow(data[i]);
+    }
 };
 
 xhr.open('GET', "http://34.89.59.112:9000/Games");
@@ -24,4 +26,42 @@ function deleteGame() {
     } else {
         //dont delete game
     }
+}
+
+function newRow(data){
+    //creates a new row in the table
+    nr = document.createElement("tr");
+    //creates a column in the table for the game ID
+    gameI = document.createElement("td");
+    nr.append(gameI);
+    //creates a column in the table for the game name
+    gameN = document.createElement("td");
+    nr.append(gameN);
+    gameN.innerHTML = data.gameName;
+    //creates a column in the table for the publisher of the game
+    gamePub = document.createElement("td");
+    nr.append(gamePub);
+    gamePub.innerHTML = data.publisher;
+    //creates a column in the table for the platform of the game
+    gamePlat = document.createElement("td");
+    nr.append(gamePlat);
+    gamePlat.innerHTML = data.platform;
+    //creates a column in the table for the year the game was released
+    gameYear = document.createElement("td");
+    nr.append(gameYear);
+    gameYear.innerHTML = data.yearOfRelease;
+    //creates a column in the table for the update game button
+    gameUpd = document.createElement("td");
+    nr.append(gameUpd);
+    upB = document.createElement("button");
+    upB.onclick = function() { updateGame() }
+    gameUpd.append(upB);
+    //creates a column in the table for the delete game button
+    gameDel = document.createElement("td");
+    nr.append(gameDel);
+    delB = document.createElement("button");
+    delB.onclick = function() { deleteGame() }
+    gameDel.append(delB);
+    //adds the new game info to the table
+    document.getElementById("gameTable").append(nr);
 }
